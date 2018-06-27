1891528c70d0f2d684aa0a1b26d7e81e3fe2432b
Scarlett Patton <scarwars05@gmail.com>
2018-06-27 02:14:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+let employeeName = ['Lisa', 'Kaitlin', 'Jan'];
+
+function printBadges(name) {
+  for (let i = 0; i < employeeName.length; i++) {
+    console.log(printBadges);
+  }
+}
+
+
+
+function tailsNeverFails() {
+  let tails = 0;
+  
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+  console.log(`You got ${tails} tails in a row!`);
+}
